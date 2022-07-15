190d41db732b51a4a697652f94e9682b539dfdad
Cagatay66 <47224774+Cagatay66@users.noreply.github.com>
2022-07-15 23:48:46 +0300

    App.js added

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,65 @@
+const Header = (props) => (
+  <>
+    <h1>{props.course.name}</h1>
+  </>
+)
+
+const Total = (props) => {
+
+  let totalnumber = 0
+
+  props.course.parts.forEach(part => {
+    totalnumber += part.exercises
+  });
+
+  return (
+    <>
+      <p>Number of exercises {totalnumber} </p>
+    </>
+  )
+}
+
+const Part = (props) => (
+  <>
+    <p>{props.part.name} {props.part.exercises} </p>
+  </>
+)
+
+const Content = (props) => (
+  <>
+    <Part part={props.course.parts[0]} />
+    <Part part={props.course.parts[1]} />
+    <Part part={props.course.parts[2]} />
+  </>
+)
+
+const App = () => {
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+export default App;
